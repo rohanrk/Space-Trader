fd89a048dc567db2b376dab5e28fad4d275beff5
haleylittmann <haleylittmann@gmail.com>
2019-02-24 17:25:40 -0500

    Deleted planet class to be able to merge with Bayne

--- app/src/main/java/com/communistutopia/spacetrader/model/Universe.kt
+++ app/src/main/java/com/communistutopia/spacetrader/model/Universe.kt
@@ -31,11 +31,7 @@ object Universe {
     }
 
     /**
-<<<<<<< HEAD
      * Generates universe with constant number of solar systems. Each solar system can have one to three planets.
-=======
-     * Generates universe with constant number of solar systems.
->>>>>>> 5f36878e7a89a1f01fe39f1976548d3dbf8d22db
      *
      * TODO: Right now, each solar system is initialized with the name 'System1', 'System2'... We will want to generate a random list of solar system and planet names to create our universe in the future.
      */
@@ -88,7 +84,7 @@ class SolarSystem(planet: Planet, name: String, x: Int, y: Int) {
         val y_dist = (this.coordinate.second - other.coordinate.second).toDouble()
         return sqrt(x_dist.pow(2) + y_dist.pow(2))
     }
-    
+
     /**
      * Method Overloading. Allows Solar System to get a distance given a point rather than
      * a solar system object
@@ -97,41 +93,6 @@ class SolarSystem(planet: Planet, name: String, x: Int, y: Int) {
         val x_dist: Double = (this.coordinate.first - loc.first).toDouble()
         val y_dist = (this.coordinate.second - loc.second).toDouble()
         return sqrt(x_dist.pow(2) + y_dist.pow(2))
-    }
-
-<<<<<<< HEAD
-    /**
-     * Method Overloading. Allows Solar System to get a distance given a point rather than
-     * a solar system object
-     */
-    fun getDistance(loc: Pair<Int, Int>): Double {
-        val x_dist: Double = (this.coordinate.first - loc.first) as Double
-        val y_dist = (this.coordinate.second - loc.second) as Double
-        return sqrt(x_dist.pow(2) + y_dist.pow(2))
-=======
-    // TODO: Solar system should be able to add planets when we generate more complex universes
-
-}
-
-/**
- * Basic Planet class
- *
- * @property name: The name of the planet
- * @property tech: Technology level of the planet. Defaulted to 0
- * @property hasPort: Whether the planet has a space port or not. Default is true
- * @author Rohan Rk
- */
-class Planet(name: String, tech: Int = 0, hasPort: Boolean = true) {
-
-    var name: String
-    var tech: Int
-    var hasSpacePort: Boolean // Default is true, but we could add random logic or make it so only one planet has a spaceport
-
-    init {
-        this.name = name
-        this.tech = tech
-        this.hasSpacePort = hasPort
->>>>>>> 5f36878e7a89a1f01fe39f1976548d3dbf8d22db
     }
 
     // TODO: Solar system should be able to add planets when we generate more complex universes
