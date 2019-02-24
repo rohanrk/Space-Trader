6ca84f2b6ef3c88755d3438380d85bbc6f98152e
rohanrk <rohanrk2009@gmail.com>
2019-02-24 13:38:19 -0500

    Universe.kt: Add kotlin doc comments and refactor
    Changed Solar Systems collection to Set and created planets Set.
    Increases efficiency of lookups and allows solar systems to have
    a variable number of planets.
    
    Switched tech level from solar system class to planet class. Now
    each planet can have different tech levels

--- app/src/main/java/com/communistutopia/spacetrader/model/Universe.kt
+++ app/src/main/java/com/communistutopia/spacetrader/model/Universe.kt
@@ -5,7 +5,10 @@ import kotlin.math.pow
 import kotlin.math.sqrt
 
 /**
- * Singleton object that handles all the logic for generating universe and other events
+ * Singleton object representing Game universe
+ *
+ * Handles all the logic for generating universe and other events
+ * @author Rohan Rk
  */
 object Universe {
 
@@ -14,16 +17,23 @@ object Universe {
     private val Y_SIZE: Int = 100
     private val NUM_SYSTEMS: Int = 10
 
-    private val solarSystems: ArrayList<SolarSystem> = ArrayList()
+    // Objects to help generate universe and handle game events
+    private val solarSystems: HashSet<SolarSystem>
     private val universe: HashMap<Pair<Int, Int>, String> // Initializing a 2D array is the most frustrating ordeal. So this is a hacky solution
     private val random: Random
 
 
     init {
-        universe = HashMap()
+        solarSystems = hashSetOf()
+        universe = hashMapOf()
         random = Random.Default
     }
 
+    /**
+     * Generates universe with constant number of solar systems.
+     *
+     * TODO: Right now, each solar system is initialized with the name 'System1', 'System2'... We will want to generate a random list of solar system and planet names to create our universe in the future.
+     */
     fun generateUniverse() {
         var i: Int = NUM_SYSTEMS
         while (i > 0) {
@@ -40,17 +50,23 @@ object Universe {
     }
 }
 
-class SolarSystem(planet: Planet, name: String, tech: Int = 0, x: Int, y: Int) {
+/**
+ * Class containing planets, tech level, and location of a Solar System
+ * @property planets: All planets located in solar system
+ * @property name: Name of the solar system
+ * @property x: x coordinate of solar system's location
+ * @property y: y coordinate of solar system's location
+ * @author Rohan Rk
+ */
+class SolarSystem(planet: Planet, name: String, x: Int, y: Int) {
 
-    var planet: Planet
+    var planets: Set<Planet>
     var name: String
-    var tech: Int
     var coordinate: Pair<Int, Int>
 
     init {
-        this.planet = planet
+        this.planets = setOf(planet)
         this.name = name
-        this.tech = tech
         this.coordinate = Pair(x, y)
     }
 
@@ -64,15 +80,27 @@ class SolarSystem(planet: Planet, name: String, tech: Int = 0, x: Int, y: Int) {
         return sqrt(x_dist.pow(2) + y_dist.pow(2))
     }
 
+    // TODO: Solar system should be able to add planets when we generate more complex universes
+
 }
 
-class Planet(name: String, hasPort: Boolean = true) {
+/**
+ * Basic Planet class
+ *
+ * @property name: The name of the planet
+ * @property tech: Technology level of the planet. Defaulted to 0
+ * @property hasPort: Whether the planet has a space port or not. Default is true
+ * @author Rohan Rk
+ */
+class Planet(name: String, tech: Int = 0, hasPort: Boolean = true) {
 
     var name: String
-    var hasSpacePort: Boolean
+    var tech: Int
+    var hasSpacePort: Boolean // Default is true, but we could add random logic or make it so only one planet has a spaceport
 
     init {
         this.name = name
+        this.tech = tech
         this.hasSpacePort = hasPort
     }
 
