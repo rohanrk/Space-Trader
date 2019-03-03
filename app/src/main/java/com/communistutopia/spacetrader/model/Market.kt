ec6795f615add93ffcfc871e31f26f8a45b1bd6c
haleylittmann <haleylittmann@gmail.com>
2019-03-02 19:16:45 -0500

    updated total to val instead of var in market class

--- app/src/main/java/com/communistutopia/spacetrader/model/Market.kt
+++ app/src/main/java/com/communistutopia/spacetrader/model/Market.kt
@@ -14,8 +14,7 @@ class Market(val techLevel: TechLevel, val resourceLevel: ResourceLevel,
              val government: Government) {
 
     var event: Event
-    var inventory = Inventory(0,0,0,0,0,0,0,0,0,
-        0)
+    var inventory: Inventory = Inventory(0,0,0,0,0,0,0,0,0,0)
 
     init {
         event = Event.None
@@ -149,6 +148,7 @@ class Market(val techLevel: TechLevel, val resourceLevel: ResourceLevel,
 
     /**
      * A function that sells goods to a Market
+     * A player cannot sell a good if the planet's tech level is too low or if they do not have enough money
      * TODO add functionality to decrement quantity of good in player's cargo
      * TODO add functionality to increment quantity of good in this Market's inventory
      * @param player the player selling the good
@@ -156,18 +156,36 @@ class Market(val techLevel: TechLevel, val resourceLevel: ResourceLevel,
      * @param numGoods the number of things being sold
      */
     fun sell(player: Player, tradeGood: TradeGood, numGoods: Int) {
-        player.credits += (tradeGood.calculatePrice(this) * numGoods)
+        val total: Int = tradeGood.calculatePrice(this) * numGoods
+        if (canSell(tradeGood.MTLU) && player.credits >= total) {
+            player.credits += total
+        }
     }
 
     /**
      * A function that sells goods to a Market
+     * A player cannot buy items if they do not have enough cargo space.
      * TODO add functionality to increment quantity of good in player's cargo
      * TODO add functionality to decrement quantity of good in this Market's inventory
      * @param player the player selling the good
      * @param tradeGood the good being sold
      * @param numGoods the number of things being sold
      */
-    fun buy(player: Player, tradeGood: TradeGood, numGoods: Int) {
-        player.credits -= (tradeGood.calculatePrice(this) * numGoods)
+    fun buy(player: Player, tradeGood: TradeGood, numGoods: Int){
+        val total: Int = tradeGood.calculatePrice(this) * numGoods
+        if(player.availableCargo() >= numGoods) {
+            player.credits -= total
+        }
+    }
+
+    /**
+     * This method finds if an item can be sold on this planet based on this planet's tech level.
+     * If the planet's tech level is below the MTLU, the item cannot be sold.
+     */
+    fun canSell(itemMTLU: Int): Boolean {
+        if (techLevel.value() < itemMTLU) {
+            return false
+        }
+        return true
     }
 }
